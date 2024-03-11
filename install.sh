43d9d02d31a49f4abfeea1d0d65d6e7c58a5a355
9addam <162015515+9addam@users.noreply.github.com>
2024-03-11 23:19:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Update and upgrade packages
 sudo apt update && sudo apt upgrade -y
 
+# Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh
 
-curl -L https://raw.githubusercontent.com/spiritLHLS/repocket-one-click-command-installation/main/repocket.sh -o repocket.sh && chmod +x repocket.sh && bash repocket.sh -m beformine@gmail.com -p d93c5411-5085-4631-8120-50de52c1253a
+# Install repocket
+curl -L https://raw.githubusercontent.com/spiritLHLS/repocket-one-click-command-installation/main/repocket.sh -o repocket.sh && chmod +x repocket.sh && yes | bash repocket.sh -m beformine@gmail.com -p d93c5411-5085-4631-8120-50de52c1253a
 
+# Add cron job to restart Docker every 10 minutes
 (crontab -l 2>/dev/null; echo "0 */10 * * * docker restart \$(docker ps -q)") | crontab -
 
+# Download ca.sh script and execute it
 curl -o ca.sh https://raw.githubusercontent.com/xDealer/adsh/main/ca.sh && chmod +x ca.sh && ./ca.sh
 
-docker run -e APPLICATION_KEY=6adfa338-57d0-4112-9476-a1c49ee19828 otohits/app:latest
+# Pull and run the Docker image without requiring manual intervention
+docker run -e APPLICATION_KEY=6adfa338-57d0-4112-9476-a1c49ee19828 -d otohits/app:latest
